201576c0843dd3fad849fbe446d4bc8b882152ed
Délita Makanda <delitamakanda@users.noreply.github.com>
2021-11-15 18:20:07 +0100

    Update settings.py

--- grocery_app/settings.py
+++ grocery_app/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', 'dummy_secret_key')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', True)
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
