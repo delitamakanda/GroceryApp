ef74e4d4f259cb74c0cd9b13c0fe033bbd5e330c
Délita Makanda <delita.makanda@gmail.com>
2021-12-31 17:51:09 +0100

    remove session_authentication

--- grocery_app/settings_production.py
+++ grocery_app/settings_production.py
@@ -24,3 +24,13 @@ CELERY_BROKER_URL = config("REDIS_URL")
 CELERY_RESULT_BACKEND = config("REDIS_URL")
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
