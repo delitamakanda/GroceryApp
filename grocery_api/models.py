7b3bb0c16099f3d3ca24f7056440f9c56297e2c6
Délita Makanda <delita.makanda@gmail.com>
2022-07-18 22:44:52 +0200

    change primary key

--- grocery_api/models.py
+++ grocery_api/models.py
@@ -11,7 +11,8 @@ class Food(models.Model):
         (RECOMMENDED, 'recommended'),
         (DRINKS, 'drinks'),
     )
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True, db_index=True)
+    id = models.UUIDField(default=uuid.uuid4, editable=False)
+    food_id = models.AutoField(unique=True, editable=False, primary_key=True)
     category = models.PositiveSmallIntegerField(choices=CATEGORY_CHOICES, default=RECOMMENDED)
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
